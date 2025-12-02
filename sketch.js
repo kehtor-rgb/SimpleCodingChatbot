3a382147718d100bf0a6c30a61e5bc8edc265c97
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2025-12-02 15:11:49 +0100

    Code Cleanup and Documentation.

--- sketch.js
+++ sketch.js
@@ -7,49 +7,46 @@ const MODEL = "mistral-nemo:12b";
 
 function setup(){
     createCanvas(300, 300);
-    console.log("Loaded p5sketch");
+    background(200);
+
     let brainScripts = ['SimCo_Brain.rive'];
     let bot = new RiveScript();
     bot.loadFile(brainScripts).then(brainReady).catch(BrainError);
-    randomCol = round(random(0, 255));
 
     let button = select('#submit');
     let user_input = select('#user_input');
     let output = select('#output');
 
-    button.mousePressed(chat);
+    button.mousePressed(postUserInput);
 
-    
     function brainReady(){
         console.log("Chatbot ready");
         bot.sortReplies();
-        let num = floor(random(10000));
-        console.log(num);
-        let reply = bot.reply('local-user', 'set '+ num);
     }
 
     function BrainError(){
         console.log("Chatbot error");
     }
 
-
-    function chat() {
+    //Posts the User Input to the Rivescript Bot,
+    //then Calls parseBotResponse on its Reply.
+    function postUserInput() {
         let input = user_input.value();
-        let reply = bot.reply("local-user", input).then(respond);
+        bot.reply("local-user", input).then(parseBotResponse);
     }
 
-    async function respond(reply){
-          console.log(reply); //send the response to the console
-          let botText = reply;
+    //Checks the Supplied Reply for the Rivescript Error Message and 
+    //if Detected, Queries Ollama with the same User Input.
+    //Then Displays the Reply.
+    async function parseBotResponse(reply){
           if (reply == "ERR: No Reply Matched"){
-            reply = await sendMessage()
+            reply = await queryOllama()
           }
           output.html(reply);
-          randomCol = round(random(0, 255));
     }
 
-
-    async function sendMessage() {
+    //Posts the User Input to Ollama and Returns a Promise of the Reply.
+    async function queryOllama() {
       let prompt = user_input.value();
       if (prompt.trim() === '') return;
       
@@ -73,10 +70,3 @@ function setup(){
       }
     }
 }
-
-
-function draw(){
-    background(randomCol);
-}
-
-
